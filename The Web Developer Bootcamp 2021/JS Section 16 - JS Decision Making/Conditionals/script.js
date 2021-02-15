11a4553971612f66cc000450a1a536a50ba708d0
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-15 23:57:54 +0100

    Update script.js

--- The Web Developer Bootcamp 2021/JS Section 16 - JS Decision Making/Conditionals/script.js
+++ The Web Developer Bootcamp 2021/JS Section 16 - JS Decision Making/Conditionals/script.js
@@ -10,7 +10,7 @@ if (random >= 0.5) {
     console.log(random);
 } */
 
-const dayOfWeek = "Friday";
+/* const dayOfWeek = "Friday";
 
 if (dayOfWeek === "Monday") {
     console.log("Mondays ain't that bad..");
@@ -20,4 +20,25 @@ else if (dayOfWeek === "Saturday") {
 }
 else if (dayOfWeek === "Friday") {
     console.log("It's Friday! Time to party! ");
+else {
+    console.log("MEH");
+} */
+
+// 0 - 5 - FREE
+// 5 - 10 - CHILD $10
+// 10 - 65 - ADULT $20
+// 65 + SENIOR $10
+
+const age = 5;
+if (age < 5) {
+    console.log("You're a baby. You get in for free")
+}
+else if (age < 10) {
+    console.log("You're a child. You pay $10");
+}
+else if (age < 65) {
+    console.log("You're an adult. You pay $20");
+}
+else {
+    console.log("You're a senior. You pay $10");
 }
