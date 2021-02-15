33173178c5519fdd16d78c4db597e2149640836b
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-15 23:34:01 +0100

    upload JS lesson
    added if statement

---
+++ The Web Developer Bootcamp 2021/JS Section 16 - JS Decision Making/Conditionals/script.js
@@ -0,0 +1,13 @@
+console.log("Before Conditional");
+
+let random = Math.random();
+
+if (random < 0.5) {
+    console.log("Your number is less than 0.5! ");
+    console.log(random);
+}
+
+if (random >= 0.5) {
+    console.log("Your number is greater than 0.5! ");
+    console.log(random);
+}
