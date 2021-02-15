f0d70c968b2f3ca4d9f5ad99d6e629696064e7f2
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-15 23:47:46 +0100

    Update script.js

--- The Web Developer Bootcamp 2021/JS Section 16 - JS Decision Making/Conditionals/script.js
+++ The Web Developer Bootcamp 2021/JS Section 16 - JS Decision Making/Conditionals/script.js
@@ -1,6 +1,4 @@
-console.log("Before Conditional");
-
-let random = Math.random();
+/* let random = Math.random();
 
 if (random < 0.5) {
     console.log("Your number is less than 0.5! ");
@@ -10,4 +8,16 @@ if (random < 0.5) {
 if (random >= 0.5) {
     console.log("Your number is greater than 0.5! ");
     console.log(random);
+} */
+
+const dayOfWeek = "Friday";
+
+if (dayOfWeek === "Monday") {
+    console.log("Mondays ain't that bad..");
+}
+else if (dayOfWeek === "Saturday") {
+    console.log("Saturday!");
+}
+else if (dayOfWeek === "Friday") {
+    console.log("It's Friday! Time to party! ");
 }
