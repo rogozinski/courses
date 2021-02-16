4d91046ab53830c53b312215f1a247b9dffa56f2
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-16 22:27:32 +0100

    more array methods
    splice and slice

--- The Web Developer Bootcamp 2021/JS Section 17 - JavaScript Arrays/More Array's methods/script.js
+++ The Web Developer Bootcamp 2021/JS Section 17 - JavaScript Arrays/More Array's methods/script.js
@@ -20,3 +20,31 @@ console.log(" Answer is: " + array3.indexOf("d"));
 //array.reverse() 
 console.warn("Let's reverse items order in array3");
 console.log(array3.reverse());
+
+//slice part of array. we can define from where to where we can slice our array BUT if we won't save it under some variable then we won't see any change. That's why I needed to define new variable called slicedColors
+let colors = ["red", "black", "blue", "yellow", "pink", "green"];
+console.warn("Our new array with the colors");
+console.log(colors);
+
+console.log("Lets slice some of them");
+let slicedColors = colors.slice(3);
+console.log(slicedColors);
+
+//splice
+//we're missing February
+let months = ["January", "March", "April"];
+console.warn("Months of the year: " + months);
+//we added February
+months.splice(1, 0, "February");
+console.log("After splicing: " + months);
+//oh no. we added December instead of May. Let's replace it 
+months.splice(4, 0, "December");
+console.log("After 2nd splicing: " + months);
+
+//all looks fine for me
+months.splice(4, 1, "May");
+console.log("After 3nd splicing: " + months);
+
+//nah, i don't really like a winter. Let's move straight to April
+months.splice(0, 3);
+console.log("After 4nd splicing: " + months);
