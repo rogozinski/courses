e707ccd3b34db06ad90f5d3f0db8ce66a1fb2134
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-16 14:42:02 +0100

    JS array practice

---
+++ The Web Developer Bootcamp 2021/JS Section 17 - JavaScript Arrays/Basic Arrays/script.js
@@ -0,0 +1,8 @@
+//declare Array of week days
+let daysWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+//display content of Arrat daysWeek
+console.log(daysWeek);
+
+//display first character of 3rd element of daysWeek
+console.log(daysWeek[2][0]);
