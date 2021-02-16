5a09b68e9e1a3c00a3283c9047aa0f4c92dc1c0a
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-16 13:01:03 +0100

    Update script.js
    ADDED LOGICAL OPERATOR &&

--- The Web Developer Bootcamp 2021/JS Section 16 - JS Decision Making/Nesting Conditionals/script.js
+++ The Web Developer Bootcamp 2021/JS Section 16 - JS Decision Making/Nesting Conditionals/script.js
@@ -1,16 +1,27 @@
+//CONDITIONALS LESSON
+
 //Password must be 6+ character
 //Password cannot include space
+// const password = prompt("Please enter a new password: ");
 
+// if (password.length >= 6) {
+//     if (password.indexOf(" ") === -1) {
+//         console.log("Valid Password");
+//     }
+//     else {
+//         console.log("Password cannot contain spaces!");
+//     }
+// }
+// else {
+//     console.log("PASSWORD TOO SHORT! Must be 6+ characters!");
+// }
+
+//CONDITIONALS LESSON + ADDED LOGICAL OPERATOR &&
 const password = prompt("Please enter a new password: ");
 
-if (password.length >= 6) {
-    if (password.indexOf(" ") === -1) {
-        console.log("Valid Password");
-    }
-    else {
-        console.log("Password cannot contain spaces!");
-    }
+if (password.length >= 6 && password.indexOf(" ") === -1) {
+    console.log("Valid Password");
 }
 else {
-    console.log("PASSWORD TOO SHORT! Must be 6+ characters!");
+    console.log("Incorrect format for password");
 }
