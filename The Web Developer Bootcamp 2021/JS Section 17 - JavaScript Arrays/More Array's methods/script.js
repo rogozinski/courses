abe46d204ec5ffce602adf86ee669dfb3dc95dad
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-16 18:23:52 +0100

    array methods practice
    concat, includes, indexOf, reverse

---
+++ The Web Developer Bootcamp 2021/JS Section 17 - JavaScript Arrays/More Array's methods/script.js
@@ -0,0 +1,22 @@
+//Concat - fusion of two arrays
+console.warn("Concatenation of two arrays");
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+
+const array3 = array1.concat(array2);
+
+console.log(array1);
+console.log(array2);
+console.log(array3);
+
+//Include - look for value
+console.warn("Does array3 contains the letter b?");
+console.log("Answer is: " + array3.includes("b"));
+
+//array.indexOf
+console.warn("What's index number of the letter d in the array3?");
+console.log(" Answer is: " + array3.indexOf("d"));
+
+//array.reverse() 
+console.warn("Let's reverse items order in array3");
+console.log(array3.reverse());
