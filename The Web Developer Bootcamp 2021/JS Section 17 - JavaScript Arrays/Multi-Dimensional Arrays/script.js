eadae2fc19288aac98580a068804b558c94051a6
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-17 09:35:55 +0100

    added task for JS exercise

--- The Web Developer Bootcamp 2021/JS Section 17 - JavaScript Arrays/Multi-Dimensional Arrays/script.js
+++ The Web Developer Bootcamp 2021/JS Section 17 - JavaScript Arrays/Multi-Dimensional Arrays/script.js
@@ -1,2 +1,15 @@
 const gameBoard = [["X", "O", "X"], ["O", "null", "X"], ["O", "O", "X"]];
 console.log(gameBoard);
+
+//Nested Arrays Exercise - Seats at the plane
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+//I've placed Hugo in the 4th row middle seat
+airplaneSeats[3][1] = "Hugo";
+console.log(airplaneSeats);
