597ad85c866477ebc6cab61573a985ae73d6ab2b
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-17 11:51:40 +0100

    added  Object Access Exercise

--- The Web Developer Bootcamp 2021/JS Section 18 - Object Literals/Basic/script.js
+++ The Web Developer Bootcamp 2021/JS Section 18 - Object Literals/Basic/script.js
@@ -11,3 +11,16 @@ const kitchenSink = {
     colors: ["red", "orange"]
 }
 console.log(kitchenSink);
+
+//Object Access Exercise
+
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206'
+}
+
+let fullAddress = restaurant.address + ", " + restaurant.city + ", " + restaurant.state + restaurant.zipcode;
+console.log(fullAddress);
