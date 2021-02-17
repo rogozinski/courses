27a034d67ea9b06b87451b48949d20b0aaeb9ee0
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-17 21:35:21 +0100

    Create whileLoop.js

---
+++ The Web Developer Bootcamp 2021/JS Section 19 - Repeating Stuff with Loops/WhileLoop/whileLoop.js
@@ -0,0 +1,26 @@
+//EXAMPLE
+
+let count = 0;
+
+while (count < 10) {
+    console.log(count);
+    count++;
+}
+
+/*
+//USEFULNESS OF WHILE LOOP IN GAMING WHEN WE DON'T KNOW HOW MANY TURN IT WILL TAKE TO WIN THE GAME
+while (!gameOver) {
+    //player1 move
+    //player2 move
+}
+//HERE YOU CAN PLACE MESSAGE FOR WINNER
+*/
+
+const secretPassword = "Pancakes";
+let guess = prompt("Enter a secret password!");
+
+while (guess !== secretPassword) {
+    guess = prompt("WRONG! TRY AGAIN!");
+}
+
+alert("Finally..");
