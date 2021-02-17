8a3cc67e4ea37d6b77c16fab22604b6e3ced5025
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-17 13:12:55 +0100

    arrays and object nesting

---
+++ The Web Developer Bootcamp 2021/JS Section 18 - Object Literals/nesting/script.js
@@ -0,0 +1,14 @@
+const comments = [
+    {
+        username: "Tammy",
+        text: "lololo",
+        votes: 9
+    },
+    {
+        username: "Guts",
+        text: "Garen from League of Legends is my twin brother",
+        votes: 9001
+    }
+]
+console.log(comments[1].username + " said: " + comments[1].text);
+comments[1].text
