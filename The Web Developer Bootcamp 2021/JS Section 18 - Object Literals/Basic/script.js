36e0d2b7bcfda66d64a0fe12e5ea86bf9a219657
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-17 11:15:43 +0100

    objects

---
+++ The Web Developer Bootcamp 2021/JS Section 18 - Object Literals/Basic/script.js
@@ -0,0 +1,13 @@
+//Simple declarations of objects
+const person = {
+    firstName: "Mick",
+    lastName: "Jagger"
+}
+console.log(person);
+
+const kitchenSink = {
+    favNum: 09319024,
+    isFunny: true,
+    colors: ["red", "orange"]
+}
+console.log(kitchenSink);
