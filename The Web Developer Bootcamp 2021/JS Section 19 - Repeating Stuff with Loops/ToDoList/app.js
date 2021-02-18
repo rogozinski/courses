dfe79d666196e6f888fcf39cc9f441659b8ea52c
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-18 17:40:46 +0100

    update todo project
    now we can add new tasks to our todo list

--- The Web Developer Bootcamp 2021/JS Section 19 - Repeating Stuff with Loops/ToDoList/app.js
+++ The Web Developer Bootcamp 2021/JS Section 19 - Repeating Stuff with Loops/ToDoList/app.js
@@ -13,6 +13,12 @@ while (input !== "quit" && input !== "q") {
             console.log(i, todoMemory[i]);
         }
         console.log("***************");
+    } else if (input === "new") {
+        const newTodo = prompt("Ok. What would you like to add?");
+        //add new element to the array
+        todoMemory.push(newTodo);
+        //display confirmation
+        console.log(`"${newTodo}" added to the list`);
     }
     //once task is accomplished ask what else we can do
     input = prompt("What would you like to do?");
