926a42657103c9966f3d6abdf8c17cd2ec3aeba2
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-18 14:20:14 +0100

    iterating over objects practice

---
+++ The Web Developer Bootcamp 2021/JS Section 19 - Repeating Stuff with Loops/iterating Objects/iteratingObjects.js
@@ -0,0 +1,41 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
+// Object.keys(testScores);
+// Object.values(testScores);
+// Object.entries(testScores);
+
+// ===================================
+// USING FOR...IN TO ITERATE AN OBJECT
+// ===================================
+
+// for (let person in testScores) {
+//     console.log(person);
+// }
+
+for (let person in testScores) {
+    console.log(`${person} scored ${testScores[person]}`);
+}
+
+
+// ==============================================
+// Using Object methods to iterate
+// (turn data into an array and then use for...of)
+// ==============================================
+
+let total = 0;
+let studentScore = Object.values(testScores);
+
+for (let score of studentScore) {
+    total += score;
+}
+console.log(total / studentScore.length);
