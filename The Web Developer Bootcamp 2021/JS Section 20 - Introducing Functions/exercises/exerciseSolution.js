957a9f09275d3e9f778329ae394f59441d75e608
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-20 20:44:40 +0100

    added solution to exercise 40

--- The Web Developer Bootcamp 2021/JS Section 20 - Introducing Functions/exercises/exerciseSolution.js
+++ The Web Developer Bootcamp 2021/JS Section 20 - Introducing Functions/exercises/exerciseSolution.js
@@ -35,3 +35,11 @@ function isShortsWeather(temperature) {
     }
     return false;
 }
+
+/* Exercise 40 - Last Element - Solution */
+function lastElement(x) {
+    if (x.length === 0) {
+        return null;
+    }
+    return x[x.length - 1];
+}
