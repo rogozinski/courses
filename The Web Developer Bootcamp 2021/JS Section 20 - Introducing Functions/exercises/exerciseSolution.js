c575fbde58983aa9b0973eb446915bf8914cea8a
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-20 19:23:42 +0100

    added solution to exercise 36

--- The Web Developer Bootcamp 2021/JS Section 20 - Introducing Functions/exercises/exerciseSolution.js
+++ The Web Developer Bootcamp 2021/JS Section 20 - Introducing Functions/exercises/exerciseSolution.js
@@ -1,6 +1,13 @@
-//Exercise 35 - Heart Function - Solution
+/* Exercise 35 - Heart Function - Solution */
 function printHeart() {
     console.log("<3");
 }
-
 //printHeart(); //uncomment to run
+
+/* Exercise 36 - Rant - Solution */
+function rant(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+rant("Yes, I can!");
