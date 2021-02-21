25717d6eb809497c8dd47d7b167d58da6952c12e
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-21 20:21:37 +0100

    defining our methods

---
+++ The Web Developer Bootcamp 2021/JS Section 21 - Leveling up our functions/methods/script.js
@@ -0,0 +1,24 @@
+/*
+const myMath = {
+    PI: 3.14159,
+    square: function (num) {
+        return num * num;
+    },
+    cube: function (num) {
+        //num to the third power
+        return num ** 3;
+    }
+}
+*/
+
+//SHORTCUT
+const myMath = {
+    PI: 3.14159,
+    square(num) {
+        return num * num;
+    },
+    cube(num) {
+        //num to the third power
+        return num ** 3;
+    }
+}
