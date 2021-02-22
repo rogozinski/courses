ba74c14cf83a0e5cac7ba1cb0b1d8cfa9954cfaa
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-22 20:59:06 +0100

    setTimeOut/setInterval

---
+++ The Web Developer Bootcamp 2021/JS Section 22  Callbacks & Array Methods/setTimeout.js
@@ -0,0 +1,14 @@
+/* setTimeout */
+
+// console.log("Hello!!");
+// setTimeout(() => {
+//     console.log("...are you still there?");
+// }, 3000);
+// console.log("Goodbye");
+
+/* setInterval */
+const id = setInterval(() => {
+    console.log(Math.random());
+}, 2000);
+
+//clearInterval(id); //to stop it
