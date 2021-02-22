4474691a195b2760c61e7a5fcfbf3a48bea055d8
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-22 18:12:57 +0100

    Update arrowFunc.js

--- The Web Developer Bootcamp 2021/JS Section 22  Callbacks & Array Methods/arrowFunc.js
+++ The Web Developer Bootcamp 2021/JS Section 22  Callbacks & Array Methods/arrowFunc.js
@@ -21,4 +21,30 @@ const rollDie2 = () => (
     Math.floor(Math.random() * 6) + 1
 )
 
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+//regular expression
+const newMovies = movies.map(function (movie) {
+    return `${movie.title} - ${movie.score / 10}`
+})
+
+//arrow function
+const newMovies2 = movies.map(movie => `${movie.title} - ${movie.score / 10}`)
+
 
