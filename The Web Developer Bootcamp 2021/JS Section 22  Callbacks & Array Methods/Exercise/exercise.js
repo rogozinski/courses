c3a0384b1832a4b59bbdd7bd95ce3f35e3e75c58
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-22 23:05:16 +0100

    added solution to exercise 50

--- The Web Developer Bootcamp 2021/JS Section 22  Callbacks & Array Methods/Exercise/exercise.js
+++ The Web Developer Bootcamp 2021/JS Section 22  Callbacks & Array Methods/Exercise/exercise.js
@@ -16,3 +16,10 @@ const greet = (userName) => {
 /* Exercise 49 - Filter Exercise - Solution */
 const username = ["john", "googleforlife777", "qq41241353231235dqq", "0_____0", "mad Max", "lilyeris"];
 const validUserNames = usrArray => usrArray.filter(names => names.length < 10);
+
+/* Exercise 50 - Some/Every Exercise - Solution */
+const evenNumbers = [2, 4, 6, 8];
+const oddNumbers = [1, 3, 4, 5];
+const mixNumbers = [1, 2, 3, 4];
+
+const allEvens = insArray => insArray.every(number => number % 2 === 0);
