d3455116e2f33cf137692755c35fd68e544c6f8a
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-22 18:05:43 +0100

    Update arrowFunc.js

--- The Web Developer Bootcamp 2021/JS Section 22  Callbacks & Array Methods/arrowFunc.js
+++ The Web Developer Bootcamp 2021/JS Section 22  Callbacks & Array Methods/arrowFunc.js
@@ -5,11 +5,20 @@
 const add = (x, y) => {
     return x + y;
 }
+const add2 = (x, y) => x + y;
 
 const square = (x) => {
     return x * x;
 }
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) + 1;
-}
+//arrow function with parens
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+const rollDie = () => Math.floor(Math.random() * 6) + 1;
+const rollDie2 = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+
