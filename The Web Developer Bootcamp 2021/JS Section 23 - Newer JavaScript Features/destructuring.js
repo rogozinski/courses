f04d5863d55448d92e3070556296b760f60d5c73
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-24 21:36:15 +0100

    destructuring params

--- The Web Developer Bootcamp 2021/JS Section 23 - Newer JavaScript Features/destructuring.js
+++ The Web Developer Bootcamp 2021/JS Section 23 - Newer JavaScript Features/destructuring.js
@@ -46,3 +46,78 @@ const user2 = {
 }
 
 // const { city, state, died = "N/A" } = user2;
+
+/* DESTRUCTURING PARAMS */
+//EXAMPLE 1
+// function fullName(user) {
+//     return `${user.firstName} ${user.lastName}`;
+// }
+// function fullName(user) {
+//     const { firstName, lastName } = user;
+//     return `${user.firstName} ${user.lastName}`;
+// }
+function fullName({ firstName, lastName = "Smith" }) {
+    return `${user.firstName} ${user.lastName}`;
+}
+
+
+//EXAMPLE 2
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99,
+        year: 1984
+    },
+    {
+        title: 'Sharknado',
+        score: 35,
+        year: 2013
+    },
+    {
+        title: '13 Going On 30',
+        score: 70,
+        year: 2004
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+        year: 1986
+    },
+    {
+        title: 'Waterworld',
+        score: 62,
+        year: 1995
+    },
+    {
+        title: 'Jingle All The Way',
+        score: 71,
+        year: 1996
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 2019
+    },
+    {
+        title: 'Notting Hill',
+        score: 77,
+        year: 1999
+    },
+    {
+        title: 'Alien',
+        score: 90,
+        year: 1979
+    }
+]
+
+// movies.filter((movie) => movie.score >= 90)
+// movies.filter(({ score }) => score >= 90)
+
+// movies.map(movie => {
+//     return `${movie.title} (${movie.year}) is rated ${movie.score}`
+// })
+
+movies.map(({ title, score, year }) => {
+    return `${title} (${year}) is rated ${score}`
+})
+
