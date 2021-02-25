ebdd5ab41e315e6da7a3cbd54133e846b0aa84dc
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 12:50:36 +0100

    added solution to exercise 54

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
@@ -8,3 +8,10 @@ const checkbox = document.querySelector('input[type="checkbox"]');
 
 /* Exercise 53 - Pickles Exercise - Solution */
 document.querySelector("h1 span").innerHTML = "<b>Disgusting</b>";
+
+/* Exercise 54 - Magical Forest Circle Exercise - Solution */
+document.querySelector("#container").style.textAlign = "center";
+
+const picture = document.querySelector("#container img");
+picture.style.width = "150px";
+picture.style.borderRadius = "50%";
