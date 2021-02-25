72976d9389fcbc5e595b9c368d7696b55be2621e
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 10:32:06 +0100

    getElementsBy lesson

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Selecting/app.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Selecting/app.js
@@ -1,18 +1,19 @@
 const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
+//display source of all img elements
+// for (let img of allImages) {
+//     console.log(img.src);
+// }
+
+//we can also change a source of all img elements
+// for (let img of allImages) {
+//     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+// }
 
 
-const squareImages = document.getElementsByClassName('square');
+//we can manipulate properties of object with the same class
+// const squareImages = document.getElementsByClassName("square");
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-}
-
-const links = document.querySelectorAll('p a');
-
-for (let link of links) {
-    console.log(link.href)
-}
+// for (let img of squareImages) {
+//     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+// }
