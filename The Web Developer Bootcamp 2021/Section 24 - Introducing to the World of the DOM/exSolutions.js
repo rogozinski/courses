789fd39b293feb37f5bfe05a5776fbb1f310db1a
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 12:59:15 +0100

    added solution to exercise 55

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
@@ -15,3 +15,10 @@ document.querySelector("#container").style.textAlign = "center";
 const picture = document.querySelector("#container img");
 picture.style.width = "150px";
 picture.style.borderRadius = "50%";
+
+/* Exercise 55 - Rainbow Text Exercise - Solution */
+const span = document.querySelectorAll('span');
+
+for (let i = 0; i < span.length; i++) {
+    span[i].style.color = colors[i];
+}
