0536458b86f9f0346c1aaa59095a54af31d14ad6
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 15:45:39 +0100

    added solution to exercise 57 and 54
    basically I've forgot to add solution for exercise 54 and because of that I've messed up with numeration...

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
@@ -9,17 +9,27 @@ const checkbox = document.querySelector('input[type="checkbox"]');
 /* Exercise 53 - Pickles Exercise - Solution */
 document.querySelector("h1 span").innerHTML = "<b>Disgusting</b>";
 
-/* Exercise 54 - Magical Forest Circle Exercise - Solution */
+/* Exercise 54 - Manipulating Attributes - Solution */
+document.querySelector("#egg").src = "https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg";
+document.querySelector("#egg").alt = "chicken";
+
+/* Exercise 55 - Magical Forest Circle Exercise - Solution */
 document.querySelector("#container").style.textAlign = "center";
 
 const picture = document.querySelector("#container img");
 picture.style.width = "150px";
 picture.style.borderRadius = "50%";
 
-/* Exercise 55 - Rainbow Text Exercise - Solution */
+/* Exercise 56 - Rainbow Text Exercise - Solution */
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 const span = document.querySelectorAll('span');
 
 for (let i = 0; i < span.length; i++) {
     span[i].style.color = colors[i];
+}
+
+/* Exercise 57 - classList Practice - Solution */
+const liContainer = document.querySelectorAll("li");
+for (let li of liContainer) {
+    li.classList.toggle("highlight");
 }
