d1e2557b078f73175dcf9a84c7491ac50c7209c8
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 12:40:48 +0100

    added solution to exercise 52

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
@@ -1,3 +1,7 @@
 /* Exercise 51 - getElementById Practice - Solution */
 const heading = document.getElementById("mainheading");
 const image = document.getElementById("unicorn");
+
+/* Exercise 52 - querySelector Practice - Solution */
+const doneTodos = document.querySelectorAll(".done");
+const checkbox = document.querySelector('input[type="checkbox"]');
