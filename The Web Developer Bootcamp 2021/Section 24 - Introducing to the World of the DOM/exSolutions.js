9b2ce73ffbc1bb33c7a1b3234726a4800b6e9987
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 12:41:34 +0100

    added solution to exercise 53

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/exSolutions.js
@@ -5,3 +5,6 @@ const image = document.getElementById("unicorn");
 /* Exercise 52 - querySelector Practice - Solution */
 const doneTodos = document.querySelectorAll(".done");
 const checkbox = document.querySelector('input[type="checkbox"]');
+
+/* Exercise 53 - Pickles Exercise - Solution */
+document.querySelector("h1 span").innerHTML = "<b>Disgusting</b>";
