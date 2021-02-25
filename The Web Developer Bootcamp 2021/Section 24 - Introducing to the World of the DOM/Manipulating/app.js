aa1d666a0189ef9baf9740d9c8018996324f17c3
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 11:51:15 +0100

    innerHTML, innerText, textContent lesson

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Manipulating/app.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Manipulating/app.js
@@ -1,12 +1,13 @@
-const allLinks = document.querySelectorAll('a');
+/* textContent display all elements in selected object even if they're hidden. innerText display text which is visible for the user */
+
+// const allLinks = document.querySelectorAll("a");
 
 // for (let link of allLinks) {
-//     link.innerText = 'I AM A LINK!!!!'
+//     link.innerText = "I AM A LINK!";
 // }
 
+//document.querySelector("h1").innerText = "<b>adasdads</b>"; //it won't recognize <i> as html but just as a regular text
+document.querySelector("h1").innerHTML = "<b>Colt's Chickens</b>" //this will work
 
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-}
+//we can also add content to the existing one
+document.querySelector("h1").innerHTML += "<sup><i> straight outta bootcamp</i></sup>" //this will work
