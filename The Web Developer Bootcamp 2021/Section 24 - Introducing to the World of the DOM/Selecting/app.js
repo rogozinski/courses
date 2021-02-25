fec35d2f36c912be3eb432af6d064bab297abc3a
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 11:05:47 +0100

    querySelector lesson

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Selecting/app.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Selecting/app.js
@@ -17,3 +17,10 @@ const allImages = document.getElementsByTagName('img');
 // for (let img of squareImages) {
 //     img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
 // }
+
+
+const links = document.querySelectorAll("p a");
+
+for (let link of links) {
+    console.log(link.href);
+}
