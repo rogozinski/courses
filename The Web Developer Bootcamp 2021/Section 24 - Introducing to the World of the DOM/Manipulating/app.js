d06c9c56161a5c60b2eff2fe10146da0b80daf22
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-25 12:38:05 +0100

    changing styles by JS

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Manipulating/app.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Manipulating/app.js
@@ -1,6 +1,6 @@
 /* textContent display all elements in selected object even if they're hidden. innerText display text which is visible for the user */
 
-// const allLinks = document.querySelectorAll("a");
+const allLinks = document.querySelectorAll("a");
 
 // for (let link of allLinks) {
 //     link.innerText = "I AM A LINK!";
@@ -11,3 +11,18 @@ document.querySelector("h1").innerHTML = "<b>Colt's Chickens</b>" //this will wo
 
 //we can also add content to the existing one
 document.querySelector("h1").innerHTML += "<sup><i> straight outta bootcamp</i></sup>" //this will work
+
+//changing style of links by JS
+for (let link of allLinks) {
+    link.style.color = "orange";
+    link.style.textDecorationColor = "magenta";
+    link.style.textDecorationStyle = "wavy";
+}
+
+//we want to check font-size of h1
+const h1 = document.querySelector("h1")
+console.log(h1.style.fontSize); //isnt able to display a value
+//so we can use different command to achieve that
+console.log(window.getComputedStyle(h1).fontSize); //32px
+
+
