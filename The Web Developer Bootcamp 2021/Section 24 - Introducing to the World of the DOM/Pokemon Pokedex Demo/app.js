95e3a3b77f1e59c9a2ba5f7361b3c657bc23e7c9
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-26 16:36:44 +0100

    Pokedex Demo

--- The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Pokemon Pokedex Demo/app.js
+++ The Web Developer Bootcamp 2021/Section 24 - Introducing to the World of the DOM/Pokemon Pokedex Demo/app.js
@@ -1,4 +1,18 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const baseURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+
+for (let i = 1; i <= 151; i++) {
+    const pokemonDiv = document.createElement("div");
+    pokemonDiv.classList.add("pokemon");
+    const label = document.createElement("span");
+    label.innerText = `# ${i}`;
+    const newImg = document.createElement("img");
+    newImg.src = `${baseURL}${i}.png`;
+
+    pokemonDiv.appendChild(newImg);
+    pokemonDiv.appendChild(label);
+    container.appendChild(pokemonDiv);
+}
