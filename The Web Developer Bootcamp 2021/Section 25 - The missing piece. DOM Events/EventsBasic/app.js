56e26a0f76503b5f85d4661d63a8890d2926f6b8
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-02-28 20:28:29 +0100

    event listener lesson

--- The Web Developer Bootcamp 2021/Section 25 - The missing piece. DOM Events/EventsBasic/app.js
+++ The Web Developer Bootcamp 2021/Section 25 - The missing piece. DOM Events/EventsBasic/app.js
@@ -4,10 +4,28 @@ btn.onclick = function () {
     console.log("You clicked button #2");
 }
 
-
 function scream() {
     console.log("AAAAAAAHHH");
 }
 btn.onmouseenter = scream;
-
 document.querySelector("h1").onclick = () => { alert("you clicked the H1"); }
+
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("dblclick", () => {
+    alert("CLICKED");
+});
+
+function twist() {
+    console.log("TWIST!")
+}
+function shout() {
+    console.log("SHOUT!")
+}
+
+const tasButton = document.querySelector('#tas');
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist)
+tasButton.addEventListener('click', shout)
