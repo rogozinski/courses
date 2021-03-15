d15cc3a3523ff5a337ca657ec7d010eb86ecdfad
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-15 14:23:36 +0100

    creating promises

---
+++ The Web Developer Bootcamp 2021/Section 27 - Async JavaScript/CreatingPromises/app.js
@@ -0,0 +1,25 @@
+// new Promise((resolve, reject) => {
+
+// })
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+        setTimeout(() => {
+            if (rand < 0.7) {
+                resolve("Your fake data here");
+            }
+            reject("Request error!");
+        }, 1000)
+
+    })
+}
+
+fakeRequest('/dogs/1')
+    .then((data) => {
+        console.log("Done with request!");
+        console.log("data is:", data);
+    })
+    .catch((err) => {
+        console.log("Oh no", err);
+    })
