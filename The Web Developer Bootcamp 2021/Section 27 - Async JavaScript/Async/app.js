da065accd1f9c5cc3c9678e56e40fca27d0d865b
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-15 16:19:01 +0100

    handling rejected promises

--- The Web Developer Bootcamp 2021/Section 27 - Async JavaScript/Async/app.js
+++ The Web Developer Bootcamp 2021/Section 27 - Async JavaScript/Async/app.js
@@ -86,6 +86,14 @@ const fakeRequest = (url) => {
 
 
 async function makeTwoRequests() {
-    let data1 = await fakeRequest('/page1');
-    console.log(data1);
+    try {
+        let data1 = await fakeRequest('/page1');
+        console.log(data1);
+        let data2 = await fakeRequest('/page2');
+        console.log(data2);
+    } catch (e) {
+        console.log("CAUGHT AN ERROR!")
+        console.log("error is:", e)
+    }
+
 }
