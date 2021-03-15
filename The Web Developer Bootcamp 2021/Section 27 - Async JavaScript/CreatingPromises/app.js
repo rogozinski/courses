f984c9b3d90a918bc93f899b395fffbf48444d80
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-15 14:41:44 +0100

    Update app.js

--- The Web Developer Bootcamp 2021/Section 27 - Async JavaScript/CreatingPromises/app.js
+++ The Web Developer Bootcamp 2021/Section 27 - Async JavaScript/CreatingPromises/app.js
@@ -58,7 +58,6 @@ const delayedColorChange = (color, delay) => {
     })
 }
 
-
 delayedColorChange('red', 1000)
     .then(() => delayedColorChange('orange', 1000))
     .then(() => delayedColorChange('yellow', 1000))
