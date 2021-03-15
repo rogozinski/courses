b1fc208c8cee52e29545bd564ac2a236d14aa22d
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-15 15:56:33 +0100

    Update app.js

--- The Web Developer Bootcamp 2021/Section 27 - Async JavaScript/Async/app.js
+++ The Web Developer Bootcamp 2021/Section 27 - Async JavaScript/Async/app.js
@@ -1,5 +1,8 @@
+/* SYNTAX */
 // async function hello() {
 // }
+
+/* EXAMPLE 1 */
 
 // const sing = async () => {
 //     throw new Error("async failed");
@@ -14,19 +17,21 @@
 //         console.log("Promise Rejected:");
 //         console.log(err);
 //     })
+/* EXAMPLE 2 */
+// const login = async (username, password) => {
+//     if (!username || !password) throw "Missing Credentials"
+//     if (password === "abc123") return "Welcome!"
+//     throw "Invalid Password"
+// }
 
-const login = async (username, password) => {
-    if (!username || !password) throw "Missing Credentials"
-    if (password === "abc123") return "Welcome!"
-    throw "Invalid Password"
-}
+// login("user", "abc123")
+//     .then(msg => {
+//         console.log("Logged in")
+//         console.log(msg)
+//     })
+//     .catch(err => {
+//         console.log("Error!")
+//         console.log(err)
+//     })
 
-login("user", "abc123")
-    .then(msg => {
-        console.log("Logged in")
-        console.log(msg)
-    })
-    .catch(err => {
-        console.log("Error!")
-        console.log(err)
-    })
+/* EXAMPLE 3 */
