099fd3034543c848e9e8b91191070273bc266dde
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-16 16:21:16 +0100

    Fetch API

---
+++ The Web Developer Bootcamp 2021/Section 28 - AJAX and API's/Fetch API/app.js
@@ -0,0 +1,24 @@
+/* EXAMPLE */
+/*
+fetch("https://api.cryptonator.com/api/full/btc-usd")
+    .then(res => {
+        console.log("RESPONSE, WAITING TO PARSE...", res);
+        //this return a Promise
+        return res.json();
+
+    })
+    .then(data => {
+        console.log("DATA PARSED...")
+        console.log(data.ticker.price)
+    })
+    .catch(error => {
+        console.log("Error", error);
+    })
+    */
+
+/* SAME EXAMPLE BUT WITH ASYNC FUNCTION */
+const fetchBitcoinPrice = async () => {
+    const res = await fetch("https://api.cryptonator.com/api/full/btc-usd");
+    const data = await res.json();
+    console.log(data.ticker.price)
+}
