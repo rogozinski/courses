ccedad39081df61d3af561b6b9eec79bc443b6fb
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-16 18:49:36 +0100

    get Dads Joke

--- The Web Developer Bootcamp 2021/Section 28 - AJAX and API's/Axios/app.js
+++ The Web Developer Bootcamp 2021/Section 28 - AJAX and API's/Axios/app.js
@@ -7,7 +7,7 @@
 //         console.log("Something went wrong: ", err);
 //     })
 
-/* EXAMPLE 2 */
+/*EXAMPLE 2 */
 const fetchBitcoinPrice = async () => {
     try {
         const res = await axios.get("https://api.cryptonator.com/api/full/btc-usd")
@@ -17,4 +17,29 @@ const fetchBitcoinPrice = async () => {
     }
 }
 
-fetchBitcoinPrice();
+//fetchBitcoinPrice();
+
+/*EXAMPLE 3 - DADs JOKES */
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("#newJoke");
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+}
+
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :("
+    }
+
+}
+
+button.addEventListener('click', addNewJoke)
