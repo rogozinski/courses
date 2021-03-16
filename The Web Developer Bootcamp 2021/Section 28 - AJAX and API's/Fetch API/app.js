203820dde494b1d9ef19446a5248c11897af2c11
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-16 16:23:27 +0100

    Update app.js

--- The Web Developer Bootcamp 2021/Section 28 - AJAX and API's/Fetch API/app.js
+++ The Web Developer Bootcamp 2021/Section 28 - AJAX and API's/Fetch API/app.js
@@ -18,7 +18,14 @@ fetch("https://api.cryptonator.com/api/full/btc-usd")
 
 /* SAME EXAMPLE BUT WITH ASYNC FUNCTION */
 const fetchBitcoinPrice = async () => {
-    const res = await fetch("https://api.cryptonator.com/api/full/btc-usd");
-    const data = await res.json();
-    console.log(data.ticker.price)
+    try {
+        const res = await fetch("https://api.cryptonator.com/api/full/btc-usd");
+        const data = await res.json();
+        console.log(data.ticker.price)
+    }
+    catch (error) {
+        console.log("SOMETHING WENT WRONG: ", error);
+    }
 }
+
+fetchBitcoinPrice();
