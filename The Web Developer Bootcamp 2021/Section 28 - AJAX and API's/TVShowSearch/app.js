94ab3cefa61229541d18e05f11227432fd1acf3a
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-17 13:28:43 +0100

    Update app.js

--- The Web Developer Bootcamp 2021/Section 28 - AJAX and API's/TVShowSearch/app.js
+++ The Web Developer Bootcamp 2021/Section 28 - AJAX and API's/TVShowSearch/app.js
@@ -1,5 +1,12 @@
 const form = document.querySelector("#searchForm");
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
     e.preventDefault();
-    console.log("aa")
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`);
+
+    // console.log(res.data[0].show.image.medium);
+    const img = document.createElement("IMG");
+    img.src = res.data[0].show.image.medium;
+
+    document.body.append(img);
 })
