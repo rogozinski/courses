f26716693a72a073097c9d8b44b1e63de0969911
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-20 15:34:46 +0100

    Language Guesser Challenge

--- The Web Developer Bootcamp 2021/Section 32 - Exploring Modules&The NPM Universe/LangGuesser/index.js
+++ The Web Developer Bootcamp 2021/Section 32 - Exploring Modules&The NPM Universe/LangGuesser/index.js
@@ -1,7 +1,14 @@
 const franc = require("franc");
 const langs = require("langs");
+const color = require("colors");
 
-const langCode = franc("Czesc. Mam na imie Michal");
+const userInput = process.argv[2];
+const langCode = franc(userInput);
 
-const language = langs.where("3", langCode);
-console.log(language.name);
+if (langCode === "und") {
+    console.log("Sorry, it is something wrong with inserted text..".red)
+} else {
+
+    const language = langs.where("3", langCode);
+    console.log(`The language of inserted text: ${language.name}`.green);
+}
