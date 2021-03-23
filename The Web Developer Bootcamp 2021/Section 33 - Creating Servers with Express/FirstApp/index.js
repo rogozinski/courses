c0e967b9e69274bace0c73d405159aaad7b1148d
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-23 18:32:43 +0100

    first express app
    express routing

--- The Web Developer Bootcamp 2021/Section 33 - Creating Servers with Express/FirstApp/index.js
+++ The Web Developer Bootcamp 2021/Section 33 - Creating Servers with Express/FirstApp/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 
+/*
 app.use((req, res) => {
     console.log("Received request");
     // res.send("Good evening. This is server of Development Studio Centre.");
     // res.send({ color: "red" });
     res.send("<h1>This is my webpage</h1>");
+})
+*/
+7
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
 
+app.get("/cats", (req, res) => {
+    res.send("MEOW");
+})
+
+app.get("/dogs", (req, res) => {
+    res.send("WOOF");
 })
 
 app.listen(8080, () => {
