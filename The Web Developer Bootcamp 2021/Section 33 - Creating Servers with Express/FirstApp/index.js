ef9321823fbf0af26eab1a4d7cc3dc0574d4489f
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-23 18:17:32 +0100

    first express app
    send a string message as a response

--- The Web Developer Bootcamp 2021/Section 33 - Creating Servers with Express/FirstApp/index.js
+++ The Web Developer Bootcamp 2021/Section 33 - Creating Servers with Express/FirstApp/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
     console.log("Received request");
+    res.send("Good evening. This is server of Development Studio Centre.");
 })
 
 app.listen(8080, () => {
