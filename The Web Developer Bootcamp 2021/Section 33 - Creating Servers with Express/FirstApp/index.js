b1dfdec7ef0a471b1f4cd238dfd46d2c25c329ca
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-03-23 18:21:56 +0100

    first express app
    display h1 code as a server response

--- The Web Developer Bootcamp 2021/Section 33 - Creating Servers with Express/FirstApp/index.js
+++ The Web Developer Bootcamp 2021/Section 33 - Creating Servers with Express/FirstApp/index.js
@@ -4,7 +4,9 @@ const app = express();
 app.use((req, res) => {
     console.log("Received request");
     // res.send("Good evening. This is server of Development Studio Centre.");
-    res.send({ color: "red" });
+    // res.send({ color: "red" });
+    res.send("<h1>This is my webpage</h1>");
+
 })
 
 app.listen(8080, () => {
