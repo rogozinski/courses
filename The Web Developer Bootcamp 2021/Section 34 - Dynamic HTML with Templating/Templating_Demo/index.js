2550de4271f68a5565de1f3eebfa37714eedd27a
Michal Rogozinski <36619764+rogozinski@users.noreply.github.com>
2021-04-05 10:58:36 +0200

    Update index.js

--- The Web Developer Bootcamp 2021/Section 34 - Dynamic HTML with Templating/Templating_Demo/index.js
+++ The Web Developer Bootcamp 2021/Section 34 - Dynamic HTML with Templating/Templating_Demo/index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 })
 
+app.get("/rand", (req, res) => {
+    res.render("random.ejs");
+})
+
 app.listen(3000, () => {
     console.log("Listening on port: 3000");
 })
